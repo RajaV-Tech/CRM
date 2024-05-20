901a7c13cd6ae9e7e27b9ef65f84a93abc30d31c
RajaV-Tech <rajavijayan900@gmail.com>
2024-05-20 21:59:09 +0530

    change port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const nodeMailer = require('nodemailer');
 const multer = require('multer');
 const path = require('path');
-const port = 3009;
+const port = 3001;
 
 
 app.use(cors());
